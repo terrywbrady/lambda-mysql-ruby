e4a9f16797165f55f6ea96797a89b15959507bca
Terry Brady <terrywbrady@gmail.com>
2020-04-21 22:00:41 -0700

    code db connection

--- handler.rb
+++ handler.rb
@@ -1,13 +1,10 @@
 require 'mysql2'
 def main(event:, context:)
-  # client = Mysql2::Client.new(
-  #   :host => "******.us-east-*.rds.amazonaws.com",
-  #   :username => "********",
-  #   :database=> "*******",
-  #   :password=> "**********",
-  #   :port => 3306)
-  # results = client.query("SELECT COUNT(*) FROM employees;").to_a
-  {
-    test: Mysql2::VERSION
-  }
+   client = Mysql2::Client.new(
+     :host => ENV['host'],
+     :username => ENV['username'],
+     :database=> ENV['database'],
+     :password=> ENV['password'],
+     :port => 3306)
+   results = client.query("SELECT id, name FROM inv_collections;").to_a 
 end
